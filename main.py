7c868addff5f93bbf4c872a14367da66d2f78934
Max Budko <mbudko2@gmail.com>
2021-04-25 12:00:19 +0700

    Flask setting up

--- main.py
+++ main.py
@@ -3,9 +3,19 @@ import datetime
 import os
 
 import yaml
-from flask import Flask, render_template, request, session, url_for
+from flask import Flask, render_template, request, session, url_for, redirect
 
 import mail
+import sentry_sdk
+
+sentry_sdk.init(
+    "https://7580bf0467114c7e9afe2889bfba38f6@o570645.ingest.sentry.io/5717743",
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+)
 
 app = Flask(__name__)
 setsfile = open('settings.yaml', 'r')
@@ -23,20 +33,27 @@ app.config['SECRET_KEY'] = key
 del key
 # app.config['SQLALCHEMY_DATABASE_URI'] = SETTINGS['database']['address']
 
+database = db.Database(SETTINGS['database']['id'])
 
+@app.route('/')
+def mainpage():
+    
 
+@app.route('/api')
+@app.route('/api/')
+def redir_to_main_page():
+    return redirect(url_for('/'))
 
-# manager = Manager(app)
-# db = SQLAlchemy(app)
+@app.route('/api/login_page')
+def get_login_page():
+    return ''
 
-# class User(db.Model)
-#     __tablename__ = 'users'
-#     id = db.Column(db.Integer(), primary_key=True)
-#     nick = db.Column(db.String(96), nullable=False)
-#     mobile_operator_id = db.Column(db.Integer(), nullable=False)
-#     xp = db.Column(db.Integer(), primary_key=True)
-#     data = db.Column()
+@app.route('/api/login', methods=['POST'])
+def login_page():
+    return ''
 
+@app.route('/api/init')
+    
 
 if __name__ == '__main__':
     app.run()
